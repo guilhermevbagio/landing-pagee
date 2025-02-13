d6cc597d20a7f3633d9c7bdee031272a75d7cdfb
Guilherme <guilhermevbagio@gmail.com>
2025-02-13 16:44:39 -0300

    dynamic cards, typewriter initiated (punctuation doesnt work)

--- src/components/TopicCard.vue
+++ src/components/TopicCard.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import AsciiCanvas from './AsciiCanvas.vue';
+import { ref } from 'vue'
+
+const isHovered = ref(false)
 
 defineProps({
     title: {
@@ -15,16 +18,22 @@ defineProps({
 </script>
 
 <template>
-    <div class="cursor-pointer hover:-translate-y-1 transition-all will-change-transform duration-150 ease-in-out ">
-        <div class="w-full h-fit border rounded p-4">
-            <div v-if="big" class="h-32 w-full overflow-clip relative rounded-sm border border-bright mb-4">
+    <div class="cursor-pointer hover:-translate-y-1 transition-all will-change-transform duration-150 ease-in-out"
+         @mouseover="isHovered = true" @mouseleave="isHovered = false">
+        <div class="w-full h-fit border rounded p-4"
+             :class="{'border-accent': !isHovered, 'hover:border-bright': isHovered}">
+            <div v-if="big" class="h-32 w-full overflow-clip relative rounded-sm border mb-4" :class="isHovered ? 'border-bright' : 'border-accent'">
                 <AsciiCanvas class="w-full h-full absolute"></AsciiCanvas>
             </div>
-            <h1 class="font-milker" :class="big ? 'text-accent font-milker text-4xl' : ' text-bright font-poppins text-lg'">
-                {{ title }}
+            <h1 :class="[
+                    isHovered ? 'text-bright' : (big ? 'text-accent' : 'text-bright'),
+                    'font-milker',
+                    big ? 'text-4xl' : 'text-lg',
+                    !isHovered && (big ? 'text-accent' : 'text-bright')
+                ]">
+                {{ title }} 
             </h1>
         </div>
-        <div class="self-center mx-3 border-b border-accent mt-3"></div>
+        <div class="self-center mx-2 mt-2" :class="!isHovered ? '' : ' border-b border-accent'"></div>
     </div>
-
 </template>
--- src/components/TopicPage.vue
+++ src/components/TopicPage.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { ref } from 'vue';
-import AsciiCanvas from './AsciiCanvas.vue';
+import { ref, onMounted } from 'vue';
 import TopicCard from './TopicCard.vue';
 
 const name = ref(``);
 const text = ref(``);
+const timelineContent = ref([]); // holds the timeline content
+const isLoading = ref(true); // loading state
+const NEUTRAL_PATH = '/src/assets/Timeline/';
 
-defineProps({
+const props = defineProps({
     title: {
         type: String,
         default: 'title'
@@ -15,69 +17,138 @@ defineProps({
         type: Boolean,
         default: false
     }
-})
+});
 
-function setText(namePassed, textPassed){
-  name.value = namePassed;
-  text.value = textPassed;
+ onMounted( async () =>  {
+    await retrieveTimelineContent();
+    clear();
+});
+
+function setText(namePassed, textPassed) {
+    name.value = namePassed;
+    typewriter(0, textPassed)
 }
+
+async function retrieveTimelineContent() {
+    let path = NEUTRAL_PATH + props.title + '.json';
+
+    let response = await fetch(path);
+    const text = await response.text();
+    const cleanedText = text.trim();
+    timelineContent.value = JSON.parse(cleanedText);
+    isLoading.value = false; 
+}
+
+function formatDate(dateStr) {
+    const [day, month, year] = dateStr.split('/');
+
+    const fullYear = `${year}`;
+
+    const date = new Date(fullYear, month - 1, day);
+
+    const months = [
+        "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const monthName = months[date.getMonth()];
+    const yearFull = date.getFullYear();
+
+    return `${monthName} ${yearFull}`;
+}
+
+function clear(){
+  let message = timelineContent.value.defaultMessage;
+  text.value = ''
+  setText(message.name, message.description)
+}
+
+let isTyping = false;
+let timeoutID;
+
+function typewriter(length, fullText) {
+  if (isTyping) {
+    clearTimeout(timeoutID);
+  }
+
+  isTyping = true;
+
+  function typingStep() {
+    if (length == fullText.length ) {
+      isTyping = false;
+      return;
+    }
+
+    text.value = fullText.slice(0, length + 1);
+
+    let timeoutLength  = fullText[length] == '.' ? 20 : 0.2
+
+    timeoutID = setTimeout(() => {
+      typewriter(length + 1, fullText);
+    }, timeoutLength);
+  }
+
+  typingStep();
+}
+
 
 </script>
 
 <template>
     <div class="h-screen w-screen">
         <header class="w-full bg-dark">
-            <div class="flex flex-row grid-rows-1 w-full justify-start items-baseline border-b pb-4 border-bright border-opacity-50 ">
+            <div class="flex flex-row grid-rows-1 w-full justify-start items-baseline border-b pb-4 border-bright border-opacity-50">
                 <div class="text-bright text-8xl w-[33%] ml-8">
-                  <h1 class="font-milker specialtext">
-                      {{ title }}<strong class="font-normal text-accent">'</strong>
-                  </h1>
+                    <h1 class="font-milker specialtext">
+                        {{ title }}<strong class="font-normal text-accent">'</strong>
+                    </h1>
                 </div>
                 <nav class="flex flex-row justify-end gap-16 px-8 col-start-2 w-full text-2xl">
-                  <router-link to="/" class="button-hover">home ></router-link>
-                  <router-link to="/music" class="button-hover">music ></router-link>
-                  <router-link to="/art" class="button-hover">art ></router-link>
-                  <router-link to="/code" class="button-hover">code ></router-link>
-                  <router-link to="/contact" class="button-hover">contact ></router-link>
+                    <router-link to="/" class="button-hover">home ></router-link>
+                    <router-link to="/music" class="button-hover">music ></router-link>
+                    <router-link to="/art" class="button-hover">art ></router-link>
+                    <router-link to="/code" class="button-hover">code ></router-link>
+                    <router-link to="/contact" class="button-hover">contact ></router-link>
                 </nav>
             </div>
         </header>
         <div class="flex flex-row items-start justify-center w-full h-full p-8">
-          <div class="flex flex-col gap-2 w-[33%]">
-            <h2 class="text-2xl text-accent font-poppins">{{ name }}</h2>
-            <p class="text-lg text-bright font-playfair ">{{ text }}</p>
-          </div>
-          <div class="flex flex-col gap-8 w-full  pl-8 ml-8 relative">
-            <div class="absolute h-screen rounded border border-accent w-2 rounded-b-none border-b-0 -z-10 left-0"></div>
-            <TopicCard title="HARSH. released" big @mouseover="setText(`December 2024`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five cent`)"></TopicCard>
-            <TopicCard title="GMTK game jam" @mouseover="setText(`November 2024`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text evertook a galley of type and scrambled it to make a type specimen book. It has survived not only five cent`)"></TopicCard>
-            <TopicCard title="Opia pirc is born" big @mouseover="setText(`October 2024`, `Lo is simply dummy text of the printing and typesetting industry. vertook a galley of type and scrambled it to make a type specimen book. It has survived not only five cent`)"></TopicCard>
-
-          </div>
+            <div class="flex flex-col gap-2 w-[33%]">
+                <h2 class="text-2xl text-accent font-poppins">{{ name }}</h2>
+                <p class="text-lg text-bright font-playfair typewriter">{{ text }}</p>
+            </div>
+            <div class="w-full pl-8 ml-8 relative">
+                <div class="absolute h-screen rounded border border-accent w-2 rounded-b-none border-b-0 -z-10 left-0"></div>
+                
+                <div v-if="isLoading" class="text-center text-bright">Loading...</div>
+                <ul v-else class="flex flex-col gap-8">
+                    <li v-for="(card, index) in timelineContent.cards" :key="index">
+                        <TopicCard :title="card.name" :big="card.big" @mouseover="setText(formatDate(card.time), card.description)" @mouseleave="clear()"></TopicCard>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-
-
-.button-hover{
-  transition: all 0.1s ease-in-out;
-  cursor: pointer;
-  @apply hover:opacity-50 text-bright text-lg
+.button-hover {
+    transition: all 0.1s ease-in-out;
+    cursor: pointer;
+    @apply hover:opacity-50 text-bright text-lg;
 }
 
 .ascii-gradient {
-  -webkit-mask-image: linear-gradient(to right, 
-    rgba(0, 0, 0, 1) 50%, 
-    rgba(0, 0, 0, 1) 60%, 
-    rgba(0, 0, 0, 0) 100%
-  );
-  mask-image: linear-gradient(to right, 
-    rgba(0, 0, 0, 1) 50%, 
-    rgba(0, 0, 0, 1) 60%, 
-    rgba(0, 0, 0, 0) 100%
-  );
+    -webkit-mask-image: linear-gradient(to right, 
+        rgba(0, 0, 0, 1) 50%, 
+        rgba(0, 0, 0, 1) 60%, 
+        rgba(0, 0, 0, 0) 100%
+    );
+    mask-image: linear-gradient(to right, 
+        rgba(0, 0, 0, 1) 50%, 
+        rgba(0, 0, 0, 1) 60%, 
+        rgba(0, 0, 0, 0) 100%
+    );
 }
 
 </style>
