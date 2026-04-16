b2fb1a5467e9ec026f3f88d3e9e6e76b49780aa3
guilherme <149219462+guilhermevbagio@users.noreply.github.com>
2026-04-16 11:59:21 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/',
+  plugins: [vue(), vueDevTools()],
+  base: "/landing-pagee/",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/landing_pagee/' : '/'
-})
+  publicPath: process.env.NODE_ENV === "production" ? "/landing_pagee/" : "/",
+});
